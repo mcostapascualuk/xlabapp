774b745287c2886d155843389328d13601b1b403
Miguel Costa <mig@codehousegroup.com>
2021-07-08 12:01:15 +0100

    frist

---
+++ XlabApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using XlabApp.Models;
+
+namespace XlabApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            List<Leedsbeerquest> lbqlist = new List<Leedsbeerquest>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44398/xlabapi"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    lbqlist = JsonConvert.DeserializeObject<List<Leedsbeerquest>>(apiResponse);
+                }
+            }
+            return View(lbqlist);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ XlabApp/Models/Leedsbeerquest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace XlabApp.Models
+{
+    public class Leedsbeerquest
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Url { get; set; }
+        public DateTime Date { get; set; }
+        public string Excerpt { get; set; }
+        public string Thumbnail { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Twitter { get; set; }
+        public string Stars_beer { get; set; }
+        public string Stars_atmosphere { get; set; }
+        public string Stars_amenities { get; set; }
+        public string Stars_value { get; set; }
+        public string Tags { get; set; }
+    }
+}
